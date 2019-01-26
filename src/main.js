6888a35a619805ed15da03b4498e806ce84e3f62
Catriel Muller <catrielmuller@gmail.com>
2019-01-26 06:49:11 -0300

    Scale to 100vw

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import 'phaser';
 import BootScene from './scenes/BootScene';
 import GameScene from './scenes/GameScene';
-import TitleScene from './scenes/TitleScene';
 
 const config = {
-  // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
   type: Phaser.WEBGL,
   pixelArt: true,
   roundPixels: true,
   parent: 'content',
   width: 400,
   height: 240,
+  zoom: 10,
+  autoResize: true,
   physics: {
     default: 'arcade',
     arcade: {
