19f3aa220b29d93a465991ebd16a53ce3b379df6
Patricio Marrone <marrone.patricio@gmail.com>
2019-01-26 12:11:28 -0300

    Using arcade physics body for enemy positions

--- src/sprites/EnemyPlayer.js
+++ src/sprites/EnemyPlayer.js
@@ -3,26 +3,34 @@ import socket from '../helpers/socket';
 export default class EnemyPlayer extends Phaser.GameObjects.Sprite {
   constructor(config) {
     super(config.scene, config.x, config.y - 16, config.key);
-    //config.scene.physics.world.enable(this);
+    config.scene.physics.world.enable(this);
     config.scene.add.existing(this);
     this.alive = true;
     this.anims.play('stand');
+
+    // config.scene.physics.world.enable(this);
+    config.scene.add.existing(this);
+
+    config.scene.physics.add.collider(config.scene.groundLayer, this);
+
+    this.acceleration = 0;
+    this.body.maxVelocity.x = 0;
+    this.body.maxVelocity.y = 0;
 
     // start still and wait until needed
     // this.body
     //   .setVelocity(0, 0)
     //   .setBounce(0, 0)
     //   .setCollideWorldBounds(false);
-    // this.body.allowGravity = false;
+    this.body.setAllowGravity(false);
 
     // Standard sprite is 16x16 pixels with a smaller body
-    // this.body.setSize(12, 12);
-    // this.body.offset.set(10, 12);
+    this.body.setSize(12, 12);
+    this.body.offset.set(10, 12);
   }
 
   move({ x, y, r }) {
-    this.x = x;
-    this.y = y;
+    this.body.reset(x, y);
     this.flipX = r;
   }
 }
