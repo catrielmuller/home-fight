aa4d0c2b484b5d2415c8711ac459c997e1176db3
Catriel Muller <catrielmuller@gmail.com>
2019-01-26 06:28:44 -0300

    Move to Heroku

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 console.log('Sólo me cargo 1 vez!');
 
-const socket = io('http://10.1.3.75:3005');
+const socket = io('https://home-fight-server.herokuapp.com');
 socket.on('connect', () => {
   console.log('Ud. se ha conectado!');
 });
